0d71f23009b41b91a468f68d04270b621ca806c5
Omurbek <omke_aka@mail.ru>
2022-09-06 00:15:22 +0600

    attemp1

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,10 @@
 package lang.print.gaps.task5;
 
+import java.util.logging.Logger;
+
 public class TimeConvertor {
     public void convert(float minutes) {
+        float convertToSec = minutes * 60;
+        System.err.println(convertToSec);
     }
 }
