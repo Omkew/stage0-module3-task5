8e5fe8194d873c7a8b5ddc3d63052f689220b6bb
Omurbek <omke_aka@mail.ru>
2022-09-06 00:33:30 +0600

    attemp2

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-    float calc = toBeDivided / 5;
+    double calc = toBeDivided / 5.0;
         System.out.println(calc);
     }
 }
