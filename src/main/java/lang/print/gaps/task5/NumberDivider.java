ce8744326382562ba452ca37bbcd4d4f598d18eb
Omurbek <omke_aka@mail.ru>
2022-09-06 00:23:47 +0600

    attemp2

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-    int calc = toBeDivided / 5;
+    float calc = toBeDivided / 5;
         System.out.println(calc);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -5,6 +5,6 @@ import java.util.logging.Logger;
 public class TimeConvertor {
     public void convert(float minutes) {
         float convertToSec = minutes * 60;
-        System.err.println(convertToSec);
+        System.out.println(convertToSec);
     }
 }
